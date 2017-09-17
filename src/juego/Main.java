7d9b522204a78b91dec14ad57f00a7f405023301
jotaeze95 <jotaeze95@gmail.com>
2017-09-17 16:42:19 -0300

    Primer Commit

---
+++ src/juego/Main.java
@@ -0,0 +1,24 @@
+package juego;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Snake();
+    }
+
+}
+
